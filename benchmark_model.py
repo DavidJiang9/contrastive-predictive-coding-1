0bd2ba7183cfe7242ad2326521ffd54aabbd72bb
Kaiqiang <715105546@qq.com>
2023-04-27 10:09:14 -0400

    1

--- benchmark_model.py
+++ benchmark_model.py
@@ -92,7 +92,8 @@ def benchmark_model(encoder_path, epochs, batch_size, output_dir, lr=1e-4, image
     ty = scale_to_01_range(ty)
 
     fig = plt.figure()
-    plt.scatter(tx,ty,color=plt.cm.Set1(labels[i]))
+    for i in range(tx.shape[0]):
+        plt.scatter(tx[i], ty[i],color=plt.cm.Set1(labels[i]))
     for i in range(tx.shape[0]):
         plt.text(tx[i], ty[i], str(labels[i]), color=plt.cm.Set1(labels[i]), 
                 fontdict={'weight': 'bold', 'size': 9})
