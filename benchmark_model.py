61c0cee217be92d5469c2aa2bd5e1211a2e73b24
Kaiqiang <715105546@qq.com>
2023-04-27 09:41:06 -0400

    1

--- benchmark_model.py
+++ benchmark_model.py
@@ -98,7 +98,7 @@ if __name__ == "__main__":
 
     benchmark_model(
         encoder_path='models/64x64/encoder.h5',
-        epochs=0,
+        epochs=1,
         batch_size=64,
         output_dir='models/64x64',
         lr=1e-3,
